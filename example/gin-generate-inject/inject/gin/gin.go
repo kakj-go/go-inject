dc7cb61017fd4101ecaaa5a5f447b1e2180467e7
zzw <kakjzzw@gmail.com>
2023-08-11 14:33:01 +0800

    1. support result use current result

--- example/gin-generate-inject/inject/gin/gin.go
+++ example/gin-generate-inject/inject/gin/gin.go
@@ -1,25 +1,26 @@
 //inject:github.com/gin-gonic/gin/gin.go
 package gin
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net"
+)
 
-type Engine struct {
-	RouterGroup
-}
-
-type RouterGroup struct {
-	Handlers HandlersChain
-}
-
-type HandlersChain []HandlerFunc
-
-type HandlerFunc struct{}
-
-func Default() (__injectResult0 *Engine) {
+func Default() (__injectResult0 *gin.Engine) {
 	fmt.Println("before Default")
 	defer func() {
 		fmt.Println("after Default")
 		fmt.Printf("Handlers len: %v \n", len(__injectResult0.Handlers))
 	}()
 	return nil
+}
+
+type Engine struct {
+}
+
+func (engine *Engine) prepareTrustedCIDRs() (__injectResult0 []*net.IPNet, __injectResult1 error) {
+	fmt.Println("before prepareTrustedCIDRs")
+	defer fmt.Println("after prepareTrustedCIDRs")
+	return nil, nil
 }
